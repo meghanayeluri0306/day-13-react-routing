f9b65af3942c8dbdc2de14177ece9a32aefc1af6
Meghana Yeluri <meghanayeluri0306@gmail.com>
2026-06-10 00:53:43 +0530

    Clean start

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <HashRouter>
+      <div style={{ minHeight: '100vh', backgroundColor: '#0a192f' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
